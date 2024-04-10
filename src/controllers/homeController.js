7648433fcd50247b86507df83b8296f2c1d8426d
Duy Khoa <npduykhoa@gmail.com>
2024-04-10 22:28:02 +0700

    UPDATE: update & delete user with mongooes

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,7 +3,7 @@ const { getAllUsers, getUserById, updateUserById, deleteUserById } = require('..
 const User = require('../models/user') //mongoose model
 
 const getHomepage = async (req, res) => {
-  const results = []
+  const results = await User.find({})
   return res.render('home.ejs', { listUsers: results })
 }
 
@@ -27,9 +27,8 @@ const postCreateUser = async (req, res) => {
 const postUpdateUser = async (req, res) => {
   const { email, name, city, userId } = req.body
   console.log('req.body', req.body)
-  await updateUserById(userId, email, name, city)
-
-  // res.send('Updated user successfully!')
+  // await updateUserById(userId, email, name, city)
+  await User.updateOne({ _id: userId }, { email, name, city }).exec()
   res.redirect('/')
 }
 
@@ -39,20 +38,22 @@ const getCreatePage = (req, res) => {
 
 const getUpdatePage = async (req, res) => {
   const userId = req.params.id
-  const user = await getUserById(userId)
+  // const user = await getUserById(userId)
+  const user = await User.findById(userId).exec()
   res.render('edit.ejs', { userEdit: user })
 }
 
 const postDeleteUser = async (req, res) => {
   const userId = req.params.id
-  const user = await getUserById(userId)
-  // const [results, fields] = await connection.query('DELETE FROM Users WHERE id = ?', [userId])
+  // const user = await getUserById(userId)
+  const user = await User.findById(userId).exec()
   res.render('delete.ejs', { userDelete: user })
 }
 
 const postHandleRemoveUser = async (req, res) => {
   const userId = req.body.userId
-  await deleteUserById(userId)
+  // await deleteUserById(userId)
+  await User.deleteOne({ _id: userId }).exec()
   res.redirect('/')
 }
 
